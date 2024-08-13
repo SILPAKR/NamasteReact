4d6019144ac0d1e6d14ce90573d24bb9b4fe4853
Silpa K R <silpaunni1997@gmail.com>
2024-08-13 19:01:55 +0530

    Episode 01

---
+++ app.js
@@ -0,0 +1,19 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "title", xyz: "abc" },
+//   "Hello React"
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "this h1 Tag"),
+    React.createElement("h1", {}, "this h1 Tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "this h1 Tag"),
+    React.createElement("h1", {}, "this h1 Tag"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
