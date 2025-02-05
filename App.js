8cc95e673c601cc2a72d68c693ff47deb85c2cb0
Silpa K R <silpaunni1997@gmail.com>
2025-02-06 00:26:48 +0530

    Episode 01

---
+++ App.js
@@ -0,0 +1,25 @@
+{
+  /* <div id="parent">
+  <div id="child">
+    <h1>Im h1 tag</h1>
+    <h2>Im h2 tag</h2>
+  </div>
+    <div id="child2">
+    <h1>Im h1 tag</h1>
+    <h2>Im h2 tag</h2>
+  </div>
+</div>; */
+}
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "im h1 Tag"),
+    React.createElement("h2", {}, "im h2 Tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "im h1 Tag"),
+    React.createElement("h2", {}, "im h2 Tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
